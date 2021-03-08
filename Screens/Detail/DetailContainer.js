d920d0f285a77c8951902f61f2ff8660cfbdb6da
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-09 00:25:07 +0900

    Receives detail information using the ID received as params, and delivers the information to the presenter.

--- Screens/Detail/DetailContainer.js
+++ Screens/Detail/DetailContainer.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useLayoutEffect, useState, useEffect } from "react";
+import { movieApi, tvApi } from "../../api";
+import DetailPresenter from "./DetailPresenter";
+import * as WebBrowser from "expo-web-browser";
 
-const DetailContainer = ({ navigation, route }) => {
-  console.log(navigation, route);
+const DetailContainer = ({ navigation, route: { params } }) => {
+  const { backDrop, id, overView, poster, title, vote, year } = params;
+  const [loading, setLoading] = useState(true);
+  const [results, setResults] = useState({
+    movies: [],
+    moviesError: null,
+  });
 
-  return <></>;
+  const getData = async () => {
+    const [movies, moviesError] = await movieApi.movieDetail(id);
+    setResults({
+      backDrop: movies.backdrop_path,
+      overView: movies.overview,
+      poster: movies.poster_path,
+      title: movies.original_title,
+      vote: movies.vote_average,
+      year: movies.release_date,
+      ...movies,
+    });
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({ title });
+  }, []);
+
+  const openBrowser = async (url) => {
+    await WebBrowser.openBrowserAsync(url);
+  };
+
+  return (
+    <DetailPresenter
+      results={results}
+      loading={loading}
+      openBrowser={openBrowser}
+    />
+  );
 };
 
 export default DetailContainer;

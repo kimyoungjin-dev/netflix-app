e9e338f056a5f0c57de6177931e1570b0dfdf60e
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-08 21:15:08 +0900

    It receives information from tvcontainer and renders it on the screen.

--- Screens/TV/TVPresenter.js
+++ Screens/TV/TVPresenter.js
@@ -1,7 +1,69 @@
 import React from "react";
+import Horizontal from "../../Components/Horizontal";
+import Slide from "../../Components/Slide";
+import ScrollContainer from "../../Components/SlideContents/ScrollContainer";
+import ScrollHorizontal from "../../Components/SlideContents/ScrollHorizontal";
+import ScrollSwiper from "../../Components/SlideContents/ScrollSwiper";
+import ScrollVertical from "../../Components/SlideContents/ScrollVertical";
+import Vertical from "../../Components/Vertical";
 
-const TVPresenter = () => {
-  return <div></div>;
+const TVPresenter = ({ topRated, popular, airingToday, thisweek, loading }) => {
+  return (
+    <>
+      <ScrollContainer loading={loading}>
+        <ScrollSwiper>
+          {topRated.map((show) => (
+            <Slide
+              key={show.id}
+              id={show.id}
+              poster={show.poster_path}
+              title={show.original_name}
+              year={show.first_air_date}
+              overView={show.overview}
+              backDrop={show.backdrop_path}
+            />
+          ))}
+        </ScrollSwiper>
+
+        <ScrollHorizontal title={"Popular show.."}>
+          {popular.map((show) => (
+            <Horizontal
+              key={show.id}
+              id={show.id}
+              poster={show.poster_path}
+              title={show.original_name}
+              vote={show.vote_average}
+            />
+          ))}
+        </ScrollHorizontal>
+
+        <ScrollHorizontal title={"this week show!"}>
+          {thisweek.map((show) => (
+            <Horizontal
+              key={show.id}
+              id={show.id}
+              poster={show.poster_path}
+              title={show.original_name}
+              vote={show.vote_average}
+            />
+          ))}
+        </ScrollHorizontal>
+
+        <ScrollVertical title={"Today show"}>
+          {airingToday.map((show) => (
+            <Vertical
+              key={show.id}
+              id={show.id}
+              poster={show.poster_path}
+              title={show.original_title}
+              vote={show.vote_average}
+              overView={show.overview}
+            />
+          ))}
+        </ScrollVertical>
+      </ScrollContainer>
+    </>
+  );
 };
 
 export default TVPresenter;

8ae8991685c9578c260b83b040d3257217eeb519
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-08 17:03:40 +0900

    Download what you need in advance using apploading

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from "expo-app-loading";
+import { Asset } from "expo-asset";
+import React, { useState } from "react";
+import { Image } from "react-native";
+import * as Font from "expo-font";
+import { NavigationContainer } from "@react-navigation/native";
+import { Ionicons, FontAwesome } from "@expo/vector-icons";
+import Stack from "./Navigation//Stack";
 
-export default function App() {
+const getImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+
+const getFonts = (fonts) => fonts.map((font) => [Font.loadAsync(font)]);
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+
+  const loadAssets = () => {
+    const images = getImages([
+      "https://images.unsplash.com/photo-1615149956009-f9fa32fc75e5?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+      require("./assets/loading.gif"),
+    ]);
+    const fonts = getFonts([Ionicons.font], [FontAwesome.font]);
+    return Promise.all([...images, ...fonts]);
+  };
+  const onFinish = () => setLoading(true);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      {loading ? (
+        <NavigationContainer>
+          <Stack />
+        </NavigationContainer>
+      ) : (
+        <AppLoading
+          startAsync={loadAssets}
+          onError={console.error}
+          onFinish={onFinish}
+        />
+      )}
+    </>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;

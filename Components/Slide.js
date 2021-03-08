f178adb576c9c71b6ec34d0c60cc10ef74477365
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-08 18:46:20 +0900

    Drop shadow design on slider, import play button from fontawesome.

--- Components/Slide.js
+++ Components/Slide.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components/native";
 import { apiImage } from "../api";
 import { trimText } from "./utils";
+import { FontAwesome } from "@expo/vector-icons";
 
 const Continaer = styled.View`
   justify-content: center;
@@ -35,14 +36,58 @@ const OverView = styled.Text`
   color: white;
 `;
 
-const Slide = ({ id, poster, title, year, overView }) => {
+const BottomData = styled.View`
+  top: 100;
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const PlayNowContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const PlayNow = styled.Text`
+  color: white;
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+const BackDropContainer = styled.Image`
+  border-radius: 30px;
+  height: 50px;
+  width: 50px;
+`;
+
+const Slide = ({ backDrop, id, poster, title, year, overView }) => {
   return (
-    <Continaer>
+    <Continaer
+      style={{
+        shadowOpacity: 18.75,
+        shadowRadius: 5,
+        shadowColor: "black",
+        shadowOffset: { height: 10, width: 10 },
+      }}
+    >
       <PosterContainer source={{ uri: apiImage(poster) }} />
       <Data>
         <Title>{title}</Title>
         <OverView>{trimText(overView, 40)}</OverView>
       </Data>
+      <BottomData>
+        <PlayNowContainer>
+          <FontAwesome
+            name="play-circle"
+            size={22}
+            color={"white"}
+            style={{ marginRight: 10 }}
+          />
+          <PlayNow>Play</PlayNow>
+        </PlayNowContainer>
+        <BackDropContainer source={{ uri: apiImage(backDrop) }} />
+      </BottomData>
     </Continaer>
   );
 };

64b815b53c5b93b6495a48e62ffe0937d8e26294
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-08 21:14:47 +0900

    Receive tv api information and transfer top rated, popular, airing today, this week information to tvPresenter.js.

--- Screens/TV/TVContainer.js
+++ Screens/TV/TVContainer.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { tvApi } from "../../api";
+import TVPresenter from "./TVPresenter";
 
-const TVContainer = () => {
-  return <></>;
+const MovieContainer = () => {
+  const [loading, setLoading] = useState(true);
+  const [results, setResults] = useState({
+    topRated: [],
+    popular: [],
+    airingToday: [],
+    thisweek: [],
+    topRatedError: null,
+    popularError: null,
+    airingTodayError: null,
+    thisweekError: null,
+  });
+
+  const getData = async () => {
+    const [topRated, topRatedError] = await tvApi.topRated();
+    const [popular, popularError] = await tvApi.popular();
+    const [airingToday, airingTodayError] = await tvApi.airingToday();
+    const [thisweek, thisweekError] = await tvApi.thisweek();
+
+    setResults({
+      topRated,
+      topRatedError,
+      popular,
+      popularError,
+      airingToday,
+      airingTodayError,
+      thisweek,
+      thisweekError,
+    });
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return <TVPresenter {...results} loading={loading} />;
 };
 
-export default TVContainer;
+export default MovieContainer;

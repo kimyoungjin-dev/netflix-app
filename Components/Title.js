04c5a101c6fcbaddabe7787be0429172ec77095b
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-08 19:30:41 +0900

    Create and use title components.

---
+++ Components/Title.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components/native";
+import { useFonts } from "expo-font";
+import { Text } from "react-native";
+
+const TextContainer = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  color: white;
+  margin-bottom: 10px;
+`;
+const Title = ({ title }) => {
+  const [loaded] = useFonts({
+    Noto_Sans: require("../assets/fonts/Noto_Sans_JP/NotoSansJP-Bold.otf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+  return (
+    <TextContainer style={{ fontFamily: "Noto_Sans" }}>{title}</TextContainer>
+  );
+};
+
+export default Title;

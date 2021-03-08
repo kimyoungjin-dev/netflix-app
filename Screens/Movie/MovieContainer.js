d32c0d8eeac2f16fb49805bcb4c2d5b49ddb8ef5
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-08 18:09:57 +0900

    The component that receives movie information, passes the movie information to the moviepresenter.

--- Screens/Movie/MovieContainer.js
+++ Screens/Movie/MovieContainer.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { movieApi } from "../../api";
+import MoviePresenter from "./MoviePresenter";
 
 const MovieContainer = () => {
-  return <></>;
+  const [loading, setLoading] = useState(true);
+  const [results, setResults] = useState({
+    nowPlaying: [],
+    popular: [],
+    upcoming: [],
+    nowPlayingError: null,
+    popularError: null,
+    upcomingError: null,
+  });
+
+  const getData = async () => {
+    const [nowPlaying, nowPlayingError] = await movieApi.nowPlaying();
+    const [popular, popularError] = await movieApi.popular();
+    const [upcoming, upcomingError] = await movieApi.upcoming();
+    setResults({
+      nowPlaying,
+      popular,
+      upcoming,
+      nowPlayingError,
+      popularError,
+      upcomingError,
+    });
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <>
+      <MoviePresenter {...results} loading={loading} />
+    </>
+  );
 };
 
 export default MovieContainer;

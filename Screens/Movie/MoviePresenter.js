b33b81d8f78eb8a10738a47f6080e0d21dd13785
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-08 18:10:18 +0900

    It receives movie information from the container and renders it on the screen.

--- Screens/Movie/MoviePresenter.js
+++ Screens/Movie/MoviePresenter.js
@@ -1,7 +1,48 @@
 import React from "react";
+import { ActivityIndicator, Dimensions, ScrollView } from "react-native";
+import styled from "styled-components/native";
+import Swiper from "react-native-web-swiper";
+import Slide from "../../Components/Slide";
 
-const MoviePresenter = () => {
-  return <div></div>;
+const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
+
+const Header = styled.View`
+  height: ${HEIGHT / 2}px;
+  width: 100%;
+`;
+
+const MoviePresenter = ({ loading, nowPlaying, popular, upcoming }) => {
+  console.log(nowPlaying);
+  return (
+    <ScrollView
+      contentContainerStyle={{
+        justifyContent: loading ? "center" : "flex-start",
+        flex: loading ? 1 : "auto",
+      }}
+      style={{
+        backgroundColor: "rgb(19, 18, 20)",
+      }}
+    >
+      {loading ? (
+        <ActivityIndicator color={"white"} size="large" />
+      ) : (
+        <Header>
+          <Swiper loop timeout={3} controlsEnabled={false}>
+            {nowPlaying.map((movie) => (
+              <Slide
+                key={movie.id}
+                id={movie.id}
+                poster={movie.poster_path}
+                title={movie.original_title}
+                year={movie.release_date}
+                overView={movie.overview}
+              />
+            ))}
+          </Swiper>
+        </Header>
+      )}
+    </ScrollView>
+  );
 };
 
 export default MoviePresenter;

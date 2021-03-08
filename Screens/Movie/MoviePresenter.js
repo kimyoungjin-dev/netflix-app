1caaff9fecd8bf86f6068e01c41c9e043c2a5037
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-08 19:32:34 +0900

    Import title horizontal, design style, nomplaying, render popular movie information,

--- Screens/Movie/MoviePresenter.js
+++ Screens/Movie/MoviePresenter.js
@@ -1,23 +1,36 @@
 import React from "react";
-import { ActivityIndicator, Dimensions, ScrollView } from "react-native";
+import { ActivityIndicator, Dimensions, ScrollView, Text } from "react-native";
 import styled from "styled-components/native";
 import Swiper from "react-native-web-swiper";
 import Slide from "../../Components/Slide";
+import Title from "../../Components/Title";
+import Horizontal from "../../Components/Horizontal";
+import { Ionicons, FontAwesome } from "@expo/vector-icons";
 
 const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
 
-const Header = styled.View`
-  height: ${HEIGHT / 2}px;
+const SlideContainer = styled.View`
+  height: ${HEIGHT / 2.1}px;
   width: 100%;
+  margin-bottom: 10px;
+`;
+
+const HorizontalContainer = styled.View``;
+
+const Contents = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
 `;
 
 const MoviePresenter = ({ loading, nowPlaying, popular, upcoming }) => {
-  console.log(nowPlaying);
+  console.log(popular);
   return (
     <ScrollView
       contentContainerStyle={{
         justifyContent: loading ? "center" : "flex-start",
         flex: loading ? 1 : "auto",
+        paddingLeft: 20,
+        paddingRight: 20,
       }}
       style={{
         backgroundColor: "rgb(19, 18, 20)",
@@ -26,21 +39,48 @@ const MoviePresenter = ({ loading, nowPlaying, popular, upcoming }) => {
       {loading ? (
         <ActivityIndicator color={"white"} size="large" />
       ) : (
-        <Header>
-          <Swiper loop timeout={3} controlsEnabled={false}>
-            {nowPlaying.map((movie) => (
-              <Slide
-                key={movie.id}
-                id={movie.id}
-                poster={movie.poster_path}
-                title={movie.original_title}
-                year={movie.release_date}
-                overView={movie.overview}
-                backDrop={movie.backdrop_path}
+        <>
+          <SlideContainer>
+            <Swiper
+              loop
+              timeout={3}
+              springConfig={{ speed: 2 }}
+              controlsEnabled={false}
+            >
+              {nowPlaying.map((movie) => (
+                <Slide
+                  key={movie.id}
+                  id={movie.id}
+                  poster={movie.poster_path}
+                  title={movie.original_title}
+                  year={movie.release_date}
+                  overView={movie.overview}
+                  backDrop={movie.backdrop_path}
+                />
+              ))}
+            </Swiper>
+          </SlideContainer>
+          <HorizontalContainer>
+            <Contents>
+              <Title title={"Popular Movies..."} />
+              <Ionicons
+                name="arrow-forward-circle-outline"
+                size={22}
+                color={"white"}
               />
-            ))}
-          </Swiper>
-        </Header>
+            </Contents>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              {popular.map((movie) => (
+                <Horizontal
+                  key={movie.id}
+                  id={movie.id}
+                  poster={movie.poster_path}
+                  title={movie.original_title}
+                />
+              ))}
+            </ScrollView>
+          </HorizontalContainer>
+        </>
       )}
     </ScrollView>
   );

2f048d99381d92aa403ed2ba7a684f4a7d87da8f
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-08 21:16:08 +0900

    Create SearchContainer.js
    When searching for movie information, the code is constructed so that it can be displayed according to the movie and TV. Consist of onchange, onsubmit, and functions

--- Screens/Search/SearchContainer.js
+++ Screens/Search/SearchContainer.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { movieApi, tvApi } from "../../api";
+import SearchPresenter from "./SearchPresenter";
 
 const SearchContainer = () => {
-  return <></>;
+  const [word, setWord] = useState("");
+  const [results, setResults] = useState({
+    movieSearch: [],
+    tvSearch: [],
+    movieSearchError: null,
+    tvSearchError: null,
+  });
+
+  const onChange = (text) => setWord(text);
+  const onSubmit = async () => {
+    const [movieSearch, movieSearchError] = await movieApi.search(word);
+    const [tvSearch, tvSearchError] = await tvApi.search(word);
+    setResults({
+      movieSearch,
+      movieSearchError,
+      tvSearch,
+      tvSearchError,
+    });
+  };
+
+  return (
+    <SearchPresenter
+      onChange={onChange}
+      onSubmit={onSubmit}
+      results={results}
+      word={word}
+    />
+  );
 };
 
 export default SearchContainer;

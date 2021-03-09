e4fc653cfda1d900da39b7c92edc9af6db987ec2
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-10 01:39:37 +0900

    Add home router

--- Navigation/Tabs.js
+++ Navigation/Tabs.js
@@ -1,5 +1,6 @@
 import React, { useLayoutEffect } from "react";
 import Home from "../Screens/Home";
+import Movie from "../Screens/Movie";
 import TV from "../Screens/TV";
 import Search from "../Screens/Search";
 import Discovery from "../Screens/Discovery";
@@ -20,7 +21,9 @@ export default ({ navigation, route }) => {
         tabBarIcon: ({ focused }) => {
           let iconName;
           if (route.name === "홈") {
-            iconName = focused ? "home" : "home-outline";
+            iconName = "home";
+          } else if (route.name === "영화") {
+            iconName = "movie";
           } else if (route.name === "TV") {
             iconName = "tv";
           } else if (route.name === "검색") {
@@ -32,7 +35,7 @@ export default ({ navigation, route }) => {
           return (
             <MaterialIcons
               name={iconName}
-              size={route.name === "Search" ? 28 : 22}
+              size={route.name === "Search" ? 30 : 25}
               color={focused ? "white" : "rgb(232,238,248)"}
             />
           );
@@ -40,6 +43,7 @@ export default ({ navigation, route }) => {
       })}
     >
       <Tabs.Screen name="홈" component={Home} />
+      <Tabs.Screen name="영화" component={Movie} />
       <Tabs.Screen name="TV" component={TV} />
       <Tabs.Screen name="검색" component={Search} />
       <Tabs.Screen name="저장한 컨텐츠 목록" component={Discovery} />

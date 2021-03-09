8049b2b460af7e821f8314852478b05f7cd0559a
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-09 18:10:28 +0900

    Import the icon and <StatusBar barStyle="light-content" />

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import AppLoading from "expo-app-loading";
 import { Asset } from "expo-asset";
 import React, { useState } from "react";
-import { Image } from "react-native";
+import { Image, StatusBar } from "react-native";
 import * as Font from "expo-font";
 import { NavigationContainer } from "@react-navigation/native";
-import { Ionicons, FontAwesome } from "@expo/vector-icons";
+import {
+  Ionicons,
+  FontAwesome,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 import Stack from "./Navigation//Stack";
 
 const getImages = (images) =>
@@ -26,16 +30,23 @@ const App = () => {
       "https://images.unsplash.com/photo-1615149956009-f9fa32fc75e5?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
       require("./assets/loading.gif"),
     ]);
-    const fonts = getFonts([Ionicons.font], [FontAwesome.font]);
+    const fonts = getFonts(
+      [Ionicons.font],
+      [FontAwesome.font],
+      [MaterialCommunityIcons.font]
+    );
     return Promise.all([...images, ...fonts]);
   };
   const onFinish = () => setLoading(true);
   return (
     <>
       {loading ? (
-        <NavigationContainer>
-          <Stack />
-        </NavigationContainer>
+        <>
+          <StatusBar barStyle="light-content" />
+          <NavigationContainer>
+            <Stack />
+          </NavigationContainer>
+        </>
       ) : (
         <AppLoading
           startAsync={loadAssets}

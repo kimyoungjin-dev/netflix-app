cbd04f7f727937b07fd61b2f81b415aceaf1a1fd
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-10 01:39:54 +0900

    Receive all information about movie and TV api.

--- Screens/Home/HomeContainer.js
+++ Screens/Home/HomeContainer.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { movieApi } from "../../api";
+import { movieApi, tvApi } from "../../api";
 import HomePresenter from "./HomePresenter";
 
 const MovieContainer = () => {
@@ -8,15 +8,34 @@ const MovieContainer = () => {
     nowPlaying: [],
     popular: [],
     upcoming: [],
+
     nowPlayingError: null,
     popularError: null,
     upcomingError: null,
+  });
+
+  const [showresult, setShowresult] = useState({
+    topRated: [],
+    showPopular: [],
+    airingToday: [],
+    thisweek: [],
+
+    topRatedError: null,
+    showPopularError: null,
+    airingTodayError: null,
+    thisweekError: null,
   });
 
   const getData = async () => {
     const [nowPlaying, nowPlayingError] = await movieApi.nowPlaying();
     const [popular, popularError] = await movieApi.popular();
     const [upcoming, upcomingError] = await movieApi.upcoming();
+
+    const [topRated, topRatedError] = await tvApi.topRated();
+    const [showPopular, showPopularError] = await tvApi.popular();
+    const [airingToday, airingTodayError] = await tvApi.airingToday();
+    const [thisweek, thisweekError] = await tvApi.thisweek();
+
     setResults({
       nowPlaying,
       popular,
@@ -25,6 +44,17 @@ const MovieContainer = () => {
       popularError,
       upcomingError,
     });
+
+    setShowresult({
+      topRated,
+      topRatedError,
+      showPopular,
+      showPopularError,
+      airingToday,
+      airingTodayError,
+      thisweek,
+      thisweekError,
+    });
     setLoading(false);
   };
 
@@ -32,7 +62,7 @@ const MovieContainer = () => {
     getData();
   }, []);
 
-  return <HomePresenter {...results} loading={loading} />;
+  return <HomePresenter {...results} loading={loading} {...showresult} />;
 };
 
 export default MovieContainer;

d3513a8d14b4daef15e8a043554fde3411803e47
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-10 01:41:55 +0900

    Receives movie api information and composes the screen.

---
+++ Screens/Movie/MovieContainer.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { movieApi } from "../../api";
+import MoviePresenter from "./MoviePresenter";
+
+const MovieContainer = () => {
+  const [loading, setLoading] = useState(true);
+  const [results, setResults] = useState({
+    nowPlaying: [],
+    popular: [],
+    upcoming: [],
+    movieTopRated: [],
+    nowPlayingError: null,
+    popularError: null,
+    upcomingError: null,
+    movieTopRatedError: null,
+  });
+
+  const getData = async () => {
+    const [nowPlaying, nowPlayingError] = await movieApi.nowPlaying();
+    const [popular, popularError] = await movieApi.popular();
+    const [upcoming, upcomingError] = await movieApi.upcoming();
+    const [movieTopRated, movieTopRatedError] = await movieApi.topRated();
+
+    setResults({
+      nowPlaying,
+      popular,
+      upcoming,
+      movieTopRated,
+      nowPlayingError,
+      popularError,
+      upcomingError,
+      movieTopRatedError,
+    });
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return <MoviePresenter {...results} loading={loading} />;
+};
+
+export default MovieContainer;
---
+++ Screens/Movie/MoviePresenter.js
@@ -0,0 +1,73 @@
+import React from "react";
+import Horizontal from "../../Components/Horizontal";
+import Slide from "../../Components/Slide";
+import ScrollContainer from "../../Components/SlideContents/ScrollContainer";
+import ScrollHorizontal from "../../Components/SlideContents/ScrollHorizontal";
+import ScrollSwiper from "../../Components/SlideContents/ScrollSwiper";
+import ScrollVertical from "../../Components/SlideContents/ScrollVertical";
+import Vertical from "../../Components/Vertical";
+
+const MoviePresenter = ({
+  nowPlaying,
+  popular,
+  upcoming,
+  movieTopRated,
+  loading,
+}) => {
+  return (
+    <ScrollContainer loading={loading}>
+      <ScrollSwiper>
+        {nowPlaying.map((movie) => (
+          <Slide
+            key={movie.id}
+            id={movie.id}
+            backDrop={movie.backdrop_path}
+            overView={movie.overview}
+            poster={movie.poster_path}
+            title={movie.original_title}
+            vote={movie.vote_average}
+          />
+        ))}
+      </ScrollSwiper>
+
+      <ScrollHorizontal title={"인기영화"}>
+        {popular.map((movie) => (
+          <Horizontal
+            key={movie.id}
+            id={movie.id}
+            poster={movie.poster_path}
+            title={movie.original_title}
+            vote={movie.vote_average}
+          />
+        ))}
+      </ScrollHorizontal>
+
+      <ScrollHorizontal title={"Top10 영화"}>
+        {movieTopRated.map((movie) => (
+          <Horizontal
+            key={movie.id}
+            id={movie.id}
+            poster={movie.poster_path}
+            title={movie.original_name}
+            vote={movie.vote_average}
+          />
+        ))}
+      </ScrollHorizontal>
+
+      <ScrollVertical title={"방금나온 영화"}>
+        {upcoming.map((movie) => (
+          <Vertical
+            key={movie.id}
+            id={movie.id}
+            poster={movie.poster_path}
+            title={movie.original_title}
+            vote={movie.vote_average}
+            overView={movie.overview}
+          />
+        ))}
+      </ScrollVertical>
+    </ScrollContainer>
+  );
+};
+
+export default MoviePresenter;

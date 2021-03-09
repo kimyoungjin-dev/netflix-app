2aafb0a80a3247272a987e6d0e5651fea79a7cb4
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-09 18:11:29 +0900

    Rebuilding the design

--- Screens/Movie/MoviePresenter.js
+++ Screens/Movie/MoviePresenter.js
@@ -1,36 +1,13 @@
 import React from "react";
-import { ActivityIndicator, Dimensions, ScrollView, Text } from "react-native";
-import styled from "styled-components/native";
-import Swiper from "react-native-web-swiper";
-import Slide from "../../Components/Slide";
-import Title from "../../Components/Title";
 import Horizontal from "../../Components/Horizontal";
-import Vertical from "../../Components/Vertical";
 import ScrollContainer from "../../Components/SlideContents/ScrollContainer";
-import ScrollSwiper from "../../Components/SlideContents/ScrollSwiper";
-import ScrollVertical from "../../Components/SlideContents/ScrollVertical";
 import ScrollHorizontal from "../../Components/SlideContents/ScrollHorizontal";
 
 const MoviePresenter = ({ loading, nowPlaying, popular, upcoming }) => {
   return (
     <ScrollContainer loading={loading}>
-      <ScrollSwiper>
+      <ScrollHorizontal title={"신작 영화"}>
         {nowPlaying.map((movie) => (
-          <Slide
-            key={movie.id}
-            id={movie.id}
-            backDrop={movie.backdrop_path}
-            overView={movie.overview}
-            poster={movie.poster_path}
-            title={movie.original_title}
-            vote={movie.vote_average}
-            year={movie.release_date}
-          />
-        ))}
-      </ScrollSwiper>
-
-      <ScrollHorizontal title={"Popular Movies.."}>
-        {popular.map((movie) => (
           <Horizontal
             key={movie.id}
             id={movie.id}
@@ -41,18 +18,40 @@ const MoviePresenter = ({ loading, nowPlaying, popular, upcoming }) => {
         ))}
       </ScrollHorizontal>
 
-      <ScrollVertical title={"Up coming Movies"}>
-        {upcoming.map((movie) => (
-          <Vertical
+      <ScrollHorizontal title={"인기 영화"}>
+        {popular.map((movie) => (
+          <Horizontal
             key={movie.id}
             id={movie.id}
             poster={movie.poster_path}
             title={movie.original_title}
             vote={movie.vote_average}
-            overView={movie.overview}
           />
         ))}
-      </ScrollVertical>
+      </ScrollHorizontal>
+      <ScrollHorizontal title={"방금 나온 신작영화"}>
+        {upcoming.map((movie) => (
+          <Horizontal
+            key={movie.id}
+            id={movie.id}
+            poster={movie.poster_path}
+            title={movie.original_title}
+            vote={movie.vote_average}
+          />
+        ))}
+      </ScrollHorizontal>
+
+      <ScrollHorizontal title={"아직안정함"}>
+        {popular.map((movie) => (
+          <Horizontal
+            key={movie.id}
+            id={movie.id}
+            poster={movie.poster_path}
+            title={movie.original_title}
+            vote={movie.vote_average}
+          />
+        ))}
+      </ScrollHorizontal>
     </ScrollContainer>
   );
 };

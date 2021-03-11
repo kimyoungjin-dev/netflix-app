bcc43b1154566fe7d4715f01108855be9304eb88
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-12 01:15:59 +0900

    By creating rank props, mark each movie's rank. and css design

--- Components/Horizontal.js
+++ Components/Horizontal.js
@@ -4,16 +4,22 @@ import styled from "styled-components/native";
 import { apiImage } from "../api";
 import { useNavigation } from "@react-navigation/native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useFonts } from "expo-font";
 
 const Container = styled.View`
   margin-right: 10px;
   margin-bottom: 20px;
 `;
 
-const PosterContainer = styled.Image`
+const PosterBox = styled.View`
   height: 160px;
   width: 130px;
-  border-radius: 5px;
+`;
+
+const PosterContainer = styled.Image`
+  height: 100%;
+  width: 100%;
+  border-radius: 10px;
 `;
 
 const NewEpisode = styled.View`
@@ -48,8 +54,44 @@ const BotEpisodeText = styled.Text`
   font-size: 10;
 `;
 
-const Horizontal = ({ isTV = false, id, poster, title, vote }) => {
+const Top10Container = styled.View`
+  background-color: red;
+  align-items: center;
+  justify-content: center;
+  align-items: center;
+  padding: 5px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  border-top-left-radius: 10px;
+  border-bottom-left-radius: 15px;
+`;
+
+const TopText = styled.Text`
+  color: white;
+  font-size: 14px;
+  font-weight: bold;
+  font-family: "Noto_Sans_JP";
+`;
+
+const Top10Text = styled.Text`
+  color: white;
+  font-weight: bold;
+  font-size: 19px;
+  font-family: "Noto_Sans_JP";
+`;
+
+const Horizontal = ({ isTV = false, id, poster, title, vote, rank }) => {
+  const [loaded] = useFonts({
+    Noto_Sans_JP: require("../assets/fonts/Noto_Sans_JP/NotoSansJP-Black.otf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
   const navigation = useNavigation();
+
+  console.log(rank);
 
   return (
     <TouchableOpacity
@@ -64,33 +106,45 @@ const Horizontal = ({ isTV = false, id, poster, title, vote }) => {
       }
     >
       <Container>
-        <PosterContainer source={{ uri: apiImage(poster) }} />
-        {isTV && vote > 8.5 && (
-          <>
-            <NewEpisode>
-              <TopEpisodeContents>
-                <TopEpisodeText>매주</TopEpisodeText>
-              </TopEpisodeContents>
-              <BotEpisodeContents>
-                <BotEpisodeText>새로운 에피소드</BotEpisodeText>
-              </BotEpisodeContents>
-            </NewEpisode>
-          </>
-        )}
+        <PosterBox>
+          <PosterContainer source={{ uri: apiImage(poster) }} />
 
-        {vote > 8 && (
-          <MaterialCommunityIcons
-            name="netflix"
-            size={20}
-            color="red"
-            style={{
-              paddingLeft: 20,
-              position: "absolute",
-              left: -22,
-              opacity: 0.8,
-            }}
-          />
-        )}
+          {rank === 1 && (
+            <Top10Container>
+              <Top10Container>
+                <TopText>Top</TopText>
+                <Top10Text>10</Top10Text>
+              </Top10Container>
+            </Top10Container>
+          )}
+
+          {isTV && vote > 8.5 && (
+            <>
+              <NewEpisode>
+                <TopEpisodeContents>
+                  <TopEpisodeText>매주</TopEpisodeText>
+                </TopEpisodeContents>
+                <BotEpisodeContents>
+                  <BotEpisodeText>새로운 에피소드</BotEpisodeText>
+                </BotEpisodeContents>
+              </NewEpisode>
+            </>
+          )}
+
+          {vote > 8 && (
+            <MaterialCommunityIcons
+              name="netflix"
+              size={20}
+              color="red"
+              style={{
+                paddingLeft: 20,
+                position: "absolute",
+                right: 5,
+                opacity: 0.8,
+              }}
+            />
+          )}
+        </PosterBox>
       </Container>
     </TouchableOpacity>
   );
--- Components/Vertical.js
+++ Components/Vertical.js
@@ -1,43 +1,86 @@
 import React from "react";
-import { TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 import { useNavigation } from "@react-navigation/native";
 import { apiImage } from "../api";
-import { trimText } from "./utils";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const Container = styled.View`
   flex-direction: row;
   margin-bottom: 30px;
 `;
 
-const PosterContainer = styled.Image`
+const PosterBox = styled.View`
   height: 130px;
   width: 110px;
+`;
+
+const PosterContainer = styled.Image`
+  height: 100%;
+  width: 100%;
   border-radius: 10px;
   margin-right: 20px;
 `;
 
 const Contents = styled.View`
   width: 60%;
+  margin-left: 15px;
 `;
 
 const Title = styled.Text`
   color: white;
-  font-size: 18px;
+  font-size: 22px;
   font-weight: bold;
 `;
 
 const Vote = styled.Text`
   color: white;
-  font-size: 12px;
+  font-size: 16px;
+  margin: 4px 0px;
 `;
 
 const OverView = styled.Text`
   color: white;
-  font-size: 14px;
+  font-size: 16px;
+  opacity: 0.7;
 `;
 
-const Vertical = ({ isTV = false, poster, title, vote, overView, id }) => {
+const Top10Container = styled.View`
+  background-color: red;
+  align-items: center;
+  justify-content: center;
+  align-items: center;
+  padding: 3px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  border-top-left-radius: 10px;
+  border-bottom-left-radius: 15px;
+`;
+
+const TopText = styled.Text`
+  color: white;
+  font-size: 14px;
+  font-weight: bold;
+  font-family: "Noto_Sans_JP";
+`;
+
+const Top10Text = styled.Text`
+  color: white;
+  font-weight: bold;
+  font-size: 19px;
+  font-family: "Noto_Sans_JP";
+`;
+
+const Vertical = ({
+  rank,
+  isTV = false,
+  poster,
+  title,
+  vote,
+  overView,
+  id,
+}) => {
   const navigation = useNavigation();
   return (
     <TouchableOpacity
@@ -53,11 +96,42 @@ const Vertical = ({ isTV = false, poster, title, vote, overView, id }) => {
       }
     >
       <Container>
-        <PosterContainer source={{ uri: apiImage(poster) }} />
+        <PosterBox>
+          <PosterContainer source={{ uri: apiImage(poster) }} />
+
+          {rank === 1 && (
+            <Top10Container>
+              <Top10Container>
+                <TopText>Top</TopText>
+                <Top10Text>10</Top10Text>
+              </Top10Container>
+            </Top10Container>
+          )}
+          {vote > 8 && (
+            <MaterialCommunityIcons
+              name="netflix"
+              size={24}
+              color="red"
+              style={{
+                paddingLeft: 20,
+                position: "absolute",
+                right: 0,
+                opacity: 0.8,
+              }}
+            />
+          )}
+        </PosterBox>
+
         <Contents>
           <Title>{title}</Title>
           <Vote>⭐️ {vote} /10</Vote>
-          <OverView>{trimText(overView, 145)}</OverView>
+          {overView ? (
+            <OverView numberOfLines={3}>{overView}</OverView>
+          ) : (
+            <Text style={{ color: "white", marginTop: 10 }}>
+              "줄거리가 존재하지않습니다."
+            </Text>
+          )}
         </Contents>
       </Container>
     </TouchableOpacity>

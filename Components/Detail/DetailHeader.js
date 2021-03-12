534bbf322a16dba79b4229e12e214787097054e0
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-13 01:54:04 +0900

    Configure the code to show the poster screen when there is no YouTube video information.

--- Components/Detail/DetailHeader.js
+++ Components/Detail/DetailHeader.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Dimensions } from "react-native";
 import styled from "styled-components/native";
 import { WebView } from "react-native-webview";
+import { apiImage } from "../../api";
 
 const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
+
+const Container = styled.View``;
 
 const Header = styled.View`
   height: ${HEIGHT / 2.5};
@@ -12,52 +14,34 @@ const Header = styled.View`
   justify-content: flex-end;
 `;
 
-const HeaderBottomContainer = styled.View`
-  flex-direction: row;
+const PosterContainer = styled.Image`
+  height: 100%;
   width: 100%;
-  justify-content: space-between;
-  margin-bottom: 10px;
-  padding: 0px 10px;
-  position: absolute;
-`;
-
-const ButtonContainer = styled.View`
-  background-color: black;
-  padding: 5px;
-  border-radius: 3px;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ButtonText = styled.Text`
-  color: white;
-  font-size: 18px;
-  font-weight: bold;
-  font-family: "Noto_Sans";
-`;
-
-const SpeakerContainer = styled.View`
-  padding: 5px;
-  background-color: black;
-  border-radius: 20px;
-  justify-content: center;
-  align-items: center;
 `;
 
 const DetailHeader = ({ results }) => {
+  console.log(results);
   return (
-    <Header>
-      <WebView
-        style={{ width: WIDTH / 1 }}
-        mixedContentMode="always"
-        source={{
-          uri: `https://www.youtube.com/embed/${results.videos?.results?.[0].key}`,
-        }}
-        scrollEnabled={true}
-        javaScriptEnabled={true}
-        allowsFullscreenVideo={false}
-      />
-    </Header>
+    <Container>
+      {results?.videos?.results[0] ? (
+        <Header>
+          <WebView
+            style={{ width: WIDTH / 1 }}
+            mixedContentMode="always"
+            source={{
+              uri: `https://www.youtube.com/embed/${results?.videos?.results?.[0]?.key}`,
+            }}
+            scrollEnabled={true}
+            javaScriptEnabled={true}
+            allowsFullscreenVideo={false}
+          />
+        </Header>
+      ) : (
+        <Header>
+          <PosterContainer source={{ uri: apiImage(results.poster_path) }} />
+        </Header>
+      )}
+    </Container>
   );
 };
 

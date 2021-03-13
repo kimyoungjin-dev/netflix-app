43d203222c6dcee34bb368a9881c2cf6d7ce2ddd
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-14 01:11:46 +0900

    Run the refresh function from each component in ScrollContainer,

--- Components/SlideContents/ScrollContainer.js
+++ Components/SlideContents/ScrollContainer.js
@@ -1,15 +1,37 @@
-import React from "react";
-import { ActivityIndicator, ScrollView } from "react-native";
+import React, { useState } from "react";
+import { ActivityIndicator, RefreshControl, ScrollView } from "react-native";
 
-const ScrollContainer = ({ loading, children }) => {
+const ScrollContainer = ({
+  loading,
+  children,
+  refreshing,
+  contentContainerStyle,
+}) => {
+  const [refresh, setRefresh] = useState(false);
+
+  const onFreshing = async () => {
+    setRefresh(true);
+    await refreshing();
+    setRefresh(false);
+  };
+
   return (
     <ScrollView
+      refreshControl={
+        <RefreshControl
+          onRefresh={onFreshing}
+          refreshing={refresh}
+          tintColor="white"
+        />
+      }
       contentContainerStyle={{
         justifyContent: loading ? "center" : "flex-start",
         flex: loading ? 1 : "auto",
-        paddingLeft: 5,
+        paddingLeft: 25,
+        paddingRight: 25,
         paddingBottom: 80,
         paddingTop: 10,
+        ...contentContainerStyle,
       }}
       style={{
         backgroundColor: "black",

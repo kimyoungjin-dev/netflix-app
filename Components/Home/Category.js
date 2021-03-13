efb51c09147c39dc28da27559dc9c8f395af4837
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-14 00:33:23 +0900

    Delete the TV category and the movie category, and make the category pop only on the home screen.

---
+++ Components/Home/Category.js
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components/native";
+import { TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+const CategoryContainer = styled.View`
+  padding: 10px 0px;
+  flex-direction: row;
+  background-color: black;
+  justify-content: space-around;
+`;
+
+const CategoryList = styled.View`
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+const CategoryItem = styled.Text`
+  color: white;
+  font-size: 20px;
+  padding-right: 20px;
+`;
+
+const Category = ({ movieGenre, tvGenre }) => {
+  const navigation = useNavigation();
+
+  return (
+    <CategoryContainer>
+      <CategoryList>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate("Category", {
+              movieGenre,
+              tvGenre,
+            })
+          }
+        >
+          <CategoryItem>카테고리</CategoryItem>
+        </TouchableOpacity>
+      </CategoryList>
+    </CategoryContainer>
+  );
+};
+
+export default Category;

290a2124dd9604e691767949e2014dd7e5d57248
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-15 00:42:28 +0900

    Delete the movie genre.

--- Screens/Home/HomeContainer.js
+++ Screens/Home/HomeContainer.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { movieApi, tvApi } from "../../api/api";
-import { apiTVGenre } from "../../api/TVGenreApi";
 import HomePresenter from "./HomePresenter";
 
 const HomeContainer = () => {
@@ -15,27 +14,8 @@ const HomeContainer = () => {
   const [showresult, setShowresult] = useState({
     showPopular: [],
     showCategory: [],
-    animation: [],
-    variety: [],
-    talk: [],
-    drama: [],
-    sfFantasy: [],
-    actionAdventure: [],
-    documentary: [],
-    comedy: [],
-    mystery: [],
-
     showPopularError: null,
     showCategoryError: null,
-    animationError: null,
-    varietyError: null,
-    talkError: null,
-    dramaError: null,
-    sfFantasyError: null,
-    actionAdventureError: null,
-    documentaryError: null,
-    comedyError: null,
-    mysteryError: null,
   });
 
   const getData = async () => {
@@ -47,22 +27,6 @@ const HomeContainer = () => {
     const [showPopular, showPopularError] = await tvApi.popular();
     const [showCategory, showCategoryError] = await tvApi.showGenre();
 
-    //genre
-    const [animation, animationError] = await apiTVGenre.showAnimation(16);
-    const [variety, varietyError] = await apiTVGenre.showVariety(10764);
-    const [talk, talkError] = await apiTVGenre.showTalk(10767);
-    const [drama, dramaError] = await apiTVGenre.showDrama(18);
-    const [sfFantasy, sfFantasyError] = await apiTVGenre.showSfFantasy(10765);
-    const [
-      actionAdventure,
-      actionAdventureError,
-    ] = await apiTVGenre.showActionAdventure(10759);
-    const [documentary, documentaryError] = await apiTVGenre.showDocumentary(
-      99
-    );
-    const [comedy, comedyError] = await apiTVGenre.showComedy(35);
-    const [mystery, mysteryError] = await apiTVGenre.showMystery(9648);
-
     setResults({
       moviePopular,
       moviePopularError,
@@ -72,27 +36,9 @@ const HomeContainer = () => {
 
     setShowresult({
       showPopular,
-      variety, //genre
-      talk,
-      animation,
-      drama,
-      sfFantasy,
-      actionAdventure,
-      documentary,
-      comedy,
-      mystery,
       showPopularError,
       showCategory,
       showCategoryError,
-      animationError,
-      varietyError,
-      talkError,
-      dramaError,
-      sfFantasyError,
-      actionAdventureError,
-      documentaryError,
-      comedyError,
-      mysteryError,
     });
     setLoading(false);
   };
@@ -100,7 +46,6 @@ const HomeContainer = () => {
   useEffect(() => {
     getData();
   }, []);
-
   return (
     <HomePresenter
       {...results}
--- Screens/Movie/MovieContainer.js
+++ Screens/Movie/MovieContainer.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { movieApi } from "../../api/api";
-import { apiMovieGenre } from "../../api/MovieGenreApi";
-
 import MoviePresenter from "./MoviePresenter";
 
 const MovieContainer = () => {
@@ -13,34 +11,10 @@ const MovieContainer = () => {
     upcoming: [],
     movieTopRated: [],
 
-    //genre
-    action: [],
-    adventure: [],
-    horror: [],
-    mystery: [],
-    sf: [],
-    thriller: [],
-    crime: [],
-    fantasy: [],
-    romance: [],
-
     nowPlayingError: null,
     popularError: null,
     upcomingError: null,
     movieTopRatedError: null,
-
-    //genreError
-    actionError: null,
-    adventureError: null,
-    horrorError: null,
-    mysteryError: null,
-    sfError: null,
-    thrillerError: null,
-    crimeError: null,
-    fantasyError: null,
-    romanceError: null,
-
-    //gereError
   });
 
   const getData = async () => {
@@ -49,16 +23,6 @@ const MovieContainer = () => {
     const [upcoming, upcomingError] = await movieApi.upcoming();
     const [movieTopRated, movieTopRatedError] = await movieApi.topRated();
     const [movieGenre, movieGenreError] = await movieApi.movieGenre(); //genre
-    //genre
-    const [action, actionError] = await apiMovieGenre.movieAction(28);
-    const [adventure, adventureError] = await apiMovieGenre.movieAdventure(12);
-    const [horror, horrorError] = await apiMovieGenre.movieHorror(27);
-    const [mystery, mysteryError] = await apiMovieGenre.movieMystery(9648);
-    const [sf, sfError] = await apiMovieGenre.movieSf(878);
-    const [thriller, thrillerError] = await apiMovieGenre.movieThriller(53);
-    const [crime, crimeError] = await apiMovieGenre.movieCrime(80);
-    const [fantasy, fantasyError] = await apiMovieGenre.movieFantasy(14);
-    const [romance, romanceError] = await apiMovieGenre.movieRomance(10752);
 
     setResults({
       //public
@@ -67,34 +31,11 @@ const MovieContainer = () => {
       upcoming,
       movieTopRated,
 
-      //genre
-
-      action,
-      adventure,
-      horror,
-      mystery,
-      sf,
-      thriller,
-      crime,
-      fantasy,
-      romance,
-
       //error
       nowPlayingError,
       popularError,
       upcomingError,
       movieTopRatedError,
-
-      //genreError
-      actionError,
-      adventureError,
-      horrorError,
-      mysteryError,
-      sfError,
-      thrillerError,
-      crimeError,
-      fantasyError,
-      romanceError,
     });
     setLoading(false);
   };

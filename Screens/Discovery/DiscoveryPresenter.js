d44ea2a288eae67fa5bdce52d0b86d05075bde73
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-14 18:40:55 +0900

    Add PanResponder and import animated
    Using panResponder, find out the location value of x,y coordinates as much as you dragged, and link the found x,y coordinates with animated. // Use animated.view because transform doesn't work in view. Define the styles of animated.vieew above and apply the style through the spread operator.

--- Screens/Discovery/DiscoveryPresenter.js
+++ Screens/Discovery/DiscoveryPresenter.js
@@ -1,23 +1,62 @@
 import React from "react";
-import { View } from "react-native";
+import { Dimensions, PanResponder, Animated } from "react-native";
+import { event } from "react-native-reanimated";
 import styled from "styled-components/native";
 import { apiImage } from "../../api/api";
+
+const { height: HEIGHT } = Dimensions.get("window");
 
 const Container = styled.View`
   flex: 1;
   background-color: black;
+  align-items: center;
 `;
-const Card = styled.View``;
+const Card = styled.View`
+  top: 30;
+  height: ${HEIGHT / 1.5}px;
+  width: 90%;
+  position: absolute;
+`;
 
-const PosterContainer = styled.Image``;
+const PosterContainer = styled.Image`
+  border-radius: 20px;
+  height: 100%;
+  width: 100%;
+`;
 
+///////styles
+
+const styles = {
+  top: 30,
+  height: HEIGHT / 1.5,
+  width: "90%",
+  position: "absolute",
+};
+
+///////components
 const DiscoveryPresenter = ({ discovery }) => {
-  console.log(discovery);
+  const position = new Animated.ValueXY();
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: (event, { dx, dy }) => {
+      position.setValue({ x: dx, y: dy });
+    },
+  });
+
   return (
     <Container>
-      <Card>
-        <PosterContainer source={{ uri: apiImage() }} />
-      </Card>
+      {discovery.reverse().map((movie) => (
+        <Animated.View
+          style={{
+            ...styles,
+            transform: [...position.getTranslateTransform()],
+          }}
+          key={movie.id}
+          {...panResponder.panHandlers}
+        >
+          <PosterContainer source={{ uri: apiImage(movie.poster_path) }} />
+        </Animated.View>
+      ))}
     </Container>
   );
 };

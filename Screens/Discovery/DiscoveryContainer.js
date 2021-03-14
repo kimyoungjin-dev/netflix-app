9bd1a814b0393b6214c3e716582db9218f9e6d97
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-14 16:20:06 +0900

    Load discovery from api.

--- Screens/Discovery/DiscoveryContainer.js
+++ Screens/Discovery/DiscoveryContainer.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { movieApi } from "../../api/api";
+import DiscoveryPresenter from "./DiscoveryPresenter";
 
 const DiscoveryContainer = () => {
-  return <></>;
+  const [results, setResults] = useState({
+    discovery: [],
+    discoveryError: null,
+  });
+
+  const getData = async () => {
+    const [discovery, discoveryError] = await movieApi.discover();
+    setResults({ discovery, discoveryError });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return <DiscoveryPresenter {...results} />;
 };
 
 export default DiscoveryContainer;
--- Screens/Discovery/DiscoveryPresenter.js
+++ Screens/Discovery/DiscoveryPresenter.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { View } from "react-native";
+import styled from "styled-components/native";
+import { apiImage } from "../../api/api";
 
-const DiscoveryPresenter = () => {
-  return <></>;
+const Container = styled.View`
+  flex: 1;
+  background-color: black;
+`;
+const Card = styled.View``;
+
+const PosterContainer = styled.Image``;
+
+const DiscoveryPresenter = ({ discovery }) => {
+  console.log(discovery);
+  return (
+    <Container>
+      <Card>
+        <PosterContainer source={{ uri: apiImage() }} />
+      </Card>
+    </Container>
+  );
 };
 
 export default DiscoveryPresenter;

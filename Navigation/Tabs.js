fa1bf7a4a7606974a250146926355dcd71ce7c29
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-16 02:04:55 +0900

    Add Open and clear search.

--- Navigation/Tabs.js
+++ Navigation/Tabs.js
@@ -2,21 +2,26 @@ import React, { useLayoutEffect } from "react";
 import Home from "../Screens/Home";
 import Movie from "../Screens/Movie";
 import TV from "../Screens/TV";
-import Search from "../Screens/Search";
+import Open from "../Screens/Open";
 import Discovery from "../Screens/Discovery";
-import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { FontAwesome } from "@expo/vector-icons";
 
-const Tabs = createMaterialBottomTabNavigator();
+const Tabs = createBottomTabNavigator();
 
 export default ({ navigation, route }) => {
   useLayoutEffect(() => {
-    navigation.setOptions({ title: "" });
+    navigation.setOptions({
+      title: "",
+    });
   }, [route]);
 
   return (
     <Tabs.Navigator
-      barStyle={{ backgroundColor: "rgb(23,25,30)", padding: 15 }}
+      barStyle={{
+        backgroundColor: "rgb(23,25,30)",
+        padding: 15,
+      }}
       screenOptions={({ route }) => ({
         tabBarIcon: ({ focused }) => {
           let iconName;
@@ -26,8 +31,8 @@ export default ({ navigation, route }) => {
             iconName = "film";
           } else if (route.name === "TV") {
             iconName = "tv";
-          } else if (route.name === "검색") {
-            iconName = "search";
+          } else if (route.name === "공개예정") {
+            iconName = "ravelry";
           } else if (route.name === "저장한 컨텐츠 목록") {
             iconName = "save";
           }
@@ -35,17 +40,26 @@ export default ({ navigation, route }) => {
           return (
             <FontAwesome
               name={iconName}
-              size={23}
-              color={focused ? "white" : "rgb(232,238,248)"}
+              size={22}
+              color={focused ? "white" : "gray"}
             />
           );
         },
       })}
+      tabBarOptions={{
+        activeTintColor: "white",
+        style: {
+          backgroundColor: "black",
+          borderTopColor: "black",
+          inactiveTintColor: "gray",
+          activeTintColor: "white",
+        },
+      }}
     >
+      <Tabs.Screen name="공개예정" component={Open} />
       <Tabs.Screen name="홈" component={Home} />
       <Tabs.Screen name="Movie" component={Movie} />
       <Tabs.Screen name="TV" component={TV} />
-      <Tabs.Screen name="검색" component={Search} />
       <Tabs.Screen name="저장한 컨텐츠 목록" component={Discovery} />
     </Tabs.Navigator>
   );
